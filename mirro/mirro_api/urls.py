85464f45d14cc6b2ce9a66f88cec44c685881767
isp-22-volobueva-id <irinavolobueva51@mail.ru>
2026-03-16 10:51:04 +0300

    views

--- mirro/mirro_api/urls.py
+++ mirro/mirro_api/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from mirro_api.views import get_xcsrf, users, auth, boards, qwe, boards_id, boards_id_accesses
+from mirro_api.views import get_xcsrf, users, auth, boards, qwe, boards_id, boards_id_accesses,boards_id_likes
 
 urlpatterns = [
     path('get_xcsrf/', get_xcsrf, name='get_xcsrf'),
@@ -9,4 +9,5 @@ urlpatterns = [
     path('qwe/', qwe, name='qwe'),
     path('boards/<int:pk_board>/', boards_id, name='boards_id'),
     path('boards_id_accesses/<int:pk_board>/', boards_id_accesses, name='boards_id_accesses'),
+    path('boards_id_likes/<int:pk_board>/', boards_id_likes, name='boards_id_likes'),
 ]
--- mirro/mirro_api/views.py
+++ mirro/mirro_api/views.py
@@ -7,6 +7,7 @@ from django.middleware.csrf import get_token
 from django.template.context_processors import request
 from django.utils.encoding import force_str, force_bytes
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+from django.views.decorators.csrf import csrf_exempt
 from mirro_api.models import User, Board, AccessToEdit, Shape, Like
 
 
@@ -237,7 +238,6 @@ def qwe(request):
 def boards_id(request, pk_board):
     try:
         board = Board.objects.get(pk_board=pk_board)
-        print(board)
     except Board.DoesNotExist:
         return JsonResponse({'code': 404, 'message': 'Ресурс не найден'}, safe=False, status=404)
     if request.method == 'GET':
@@ -253,7 +253,6 @@ def boards_id(request, pk_board):
     if not user:
         return JsonResponse({'code': 401, 'message': 'Пользователь не авторизован'}, safe=False, status=401)
     is_owner = AccessToEdit.objects.filter(fk_board = board, fk_user = user, author = 1).exists()
-    print(is_owner)
     if not is_owner:
         return JsonResponse({'code': 403, 'message': 'Недостаточно прав'}, safe=False, status=403)
 
@@ -303,13 +302,11 @@ def boards_id_accesses(request, pk_board):
 
     try:
         board = Board.objects.get(pk_board=pk_board)
-        print(board)
     except Board.DoesNotExist:
         return JsonResponse({'code': 404, 'message': 'Ресурс не найден'}, safe=False, status=404)
 
     # Общая проверка прав: действия с доступами разрешены ТОЛЬКО владельцу (author=1), return 403
     is_owner = AccessToEdit.objects.filter(fk_board=board, fk_user=user, author=1).exists()
-    print(is_owner)
     if not is_owner:
         return JsonResponse({'code': 403, 'message': 'Недостаточно прав'}, safe=False, status=403)
 
@@ -331,33 +328,145 @@ def boards_id_accesses(request, pk_board):
 
     # # --- ДОБАВЛЕНИЕ СОАВТОРА (POST) ---
     elif request.method == 'POST':
-    # Извлекаем target_email
+        # Извлекаем target_email
         target_email = request.POST.get('email')
-        
-    # проверка существует ли пользователь с target_email
+        try:
+            user_target = User.objects.get(email=target_email)
+        except User.DoesNotExist:
+            return JsonResponse({'code': 404, 'message': 'Пользователь с таким email не найден'}, safe=False, status=404)
+        # Проверяем, нет ли уже доступа у пользователя с target_email
+        is_owner = AccessToEdit.objects.filter(fk_board=board, fk_user=user_target).exists()
+        if is_owner: # return 422
+           return JsonResponse({'code': 422, 'message': 'Доступ уже есть'}, safe=False, status=422)
+        else:
+            # Создаем запись соавтора (author=0)
+            # return 201
+            accessToEdit = AccessToEdit.objects.create(
+                fk_user = user_target,
+                fk_board = board,
+                author=0
+            )
+            return JsonResponse({'code': 201, 'message': 'Доступ пользователю предоставлен'}, safe=False, status=201)
+
+    elif request.method == 'DELETE':
+        put_data = QueryDict(request.body)
+        target_email = put_data.get('email')
+        print(target_email)
+        error422 = {
+            # 'errors': {},
+            'code': 422,
+            'message': 'Некорректные данные'
+        }
+        errors = {
+            'target_email': [],
+        }
+        if not target_email or target_email == '':
+            errors['target_email'].append("Поле не должно быть пустым")
+
+        for key in list(errors.keys()):
+            if not errors[key]:
+                del errors[key]
+
+        if errors:
+            error422['errors'] = errors
+            return JsonResponse(error422, safe=False, status=422)
+
+        if user.email == target_email:
+            return JsonResponse({'code': 403, 'message': 'Нет прав'}, safe=False, status=403)
+
+        try:
+            user_target = User.objects.get(email=target_email)
+        except User.DoesNotExist:
+            return JsonResponse({'code': 404, 'message': 'Пользователь с таким email не найден'}, safe=False,
+                                status=404)
+
+        # Проверяем, что пользователь является соавтором (author=0)
+        access_entry = AccessToEdit.objects.filter(
+            fk_board_id=pk_board,
+            fk_user=user_target,
+            author=0
+        ).first()
+
+        if not access_entry:
+            return JsonResponse({'code': 404, 'message': 'Соавтор не найден'}, safe=False, status=404)
+
+        access_entry.delete()
+        return JsonResponse({'code': 200, 'message': 'Соавтор удален'}, safe=False, status=200)
+
+
+@csrf_exempt
+def boards_id_likes(request, pk_board):
+    # получаем пользователя
+    user = is_auth(request)
+    # Общая проверка авторизован ли пользователь, return 401
+    if not user:
+        return JsonResponse({'code': 401, 'message': 'Пользователь не авторизован'}, safe=False, status=401)
+    # Общая проверка существует ли доска с таким id, return 404
+    try:
+        board = Board.objects.get(pk_board = pk_board)
+    except Board.DoesNotExist:
+        return JsonResponse({'code': 404, 'message': 'Ресурс не найден'}, safe=False, status=404)
+    # --- ПОЛУЧЕНИЕ СПИСКА ЛАЙКНУВШИХ (GET) ---
+    if request.method == 'GET':
+    # Проверка прав: только владелец (author=1) видит список тех, кто лайкнул
+        author = AccessToEdit.objects.filter(fk_user=user, author=1, fk_board=board).exists()
+        if not author:
+            # return 403
+            return JsonResponse({
+                'code': 403,
+                'message': 'Недостаточно прав'
+            }, safe=False, status=403)
+    # Получаем всех пользователей из таблицы Like для этой доски
+        likes = Like.objects.select_related('fk_user').filter(fk_board=board)
+    # fk_user, fk_board, pk_like
+        likes_list = []
+        for like in likes:
+            likes_list.append({
+                'username': like.fk_user.username,
+                'email': like.fk_user.email,
+            })
+
+        return JsonResponse({
+            'code': 200,
+            'message': 'Список пользователей поставивших лайки',
+            'data': likes_list
+        }, safe=False, status=200)
+
+    # --- ДОБАВЛЕНИЕ ЛАЙКА (POST) ---
+    elif request.method == 'POST':
+        # Проверяем, не стоит ли лайк уже
+        like = Like.objects.filter(fk_user=user, fk_board=board).exists()
+        if like:
+            return JsonResponse({'code': 422, 'message': 'Пользователь уже поставил лайк'}, safe=False, status=422)
+        # Создаем лайк
+        Like.objects.create(
+            fk_user=user,
+            fk_board=board
+        )
+        # Обновляем счетчик в доске
+        board.total_like += 1
+        board.save()
+        # return 201
+        return JsonResponse({
+            'code': 201,
+            'message': 'Лайк поставлен',
+            'data': {
+                'total_like': board.total_like
+            }
+        }, safe=False, status=201)
+
+    # --- УДАЛЕНИЕ ЛАЙКА (DELETE) ---
+    elif request.method == 'DELETE':
+        try:
+            like = Like.objects.filter(fk_user=user, fk_board=board)
+        except Like.DoesNotExist:
+            return JsonResponse({'code': 404, 'message': 'Не найдено'}, safe=False, status=404)
+    # Ищем лайк именно от текущего пользователя
     # return 404
-
-    # Проверяем, нет ли уже доступа у пользователя с target_email
-    # return 422
-
-    # Создаем запись соавтора (author=0)
-    # return 201
-
-    # --- УДАЛЕНИЕ СОАВТОРА (DELETE) ---
-    # elif request.method == 'DELETE':
-    # # Извлекаем target_email
-    #
-    # # 1. Проверка на пустой ввод
-    # # return 422
-    #
-    # # 2. Проверка на попытку удаления самого себя (владельца)
-    # # return 403
-    #
-    # # 3. Ищем пользователя по target_email
-    # # return 404
-    #
-    # # 4. Ищем запись доступа именно как соавтора (author=0) для этой доски
-    # # return 404
-    #
-    # # удаляем
-    # # return 200
+        like.delete()
+        board.total_like -= 1
+        board.save()
+        return JsonResponse({'code': 200, 'message': 'Лайк удален'}, safe=False, status=200)
+    # удаляем лайк
+    # Уменьшаем счетчик (не уходя в минус)
+    # return 200
